9d4b53fceac98025dde3759165c9111986f47e01
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-18 16:02:46 +0800

    Few changes applied

--- scripts/OutdoorChairComponent.ts
+++ scripts/OutdoorChairComponent.ts
@@ -10,7 +10,7 @@ import * as vec3 from "vec3";
 
 type CardinalDirection = "north" | "east" | "south" | "west";
 
-const SittableMob = "pafa_seat";
+const SittableMob = "pafa:seat2";
 const Sittable_ACTOR_TYPE = SittableMob;
 
 const onPlayerInteract = (arg1: BlockComponentPlayerInteractEvent, arg2: btch.CustomComponentParameters): void => {
@@ -21,14 +21,14 @@ const onPlayerInteract = (arg1: BlockComponentPlayerInteractEvent, arg2: btch.Cu
 
   const centro = block.center();
 
-  if (vec3.distance(centro, player.location) > 2.5) {
-    if (block.hasTag("outdoor_chair")) {
-      player.onScreenDisplay.setActionBar({
-        translate: "outdoor_chair.out_of_range",
-      });
-    }
-    return;
-  }
+  // if (vec3.distance(centro, player.location) > 2.5) {
+  //   if (block.hasTag("outdoor_chair")) {
+  //     player.onScreenDisplay.setActionBar({
+  //       translate: "outdoor_chair.out_of_range",
+  //     });
+  //   }
+  //   return;
+  // }
   const centralHeightY = centro.y;
 
   let height: number;
@@ -60,13 +60,13 @@ const onPlayerInteract = (arg1: BlockComponentPlayerInteractEvent, arg2: btch.Cu
 
   switch (blockDirectiones) {
     case "north":
-      SittableSpawnEvent = `sittable_anchor:r180`;
+      SittableSpawnEvent = `sittable_actor:r180`;
       break;
     case "east":
-      SittableSpawnEvent = `sittable_anchor:r270`;
+      SittableSpawnEvent = `sittable_actor:r270`;
       break;
     case "west":
-      SittableSpawnEvent = `sittable_anchor:90`;
+      SittableSpawnEvent = `sittable_actor:90`;
       break;
     default:
       SittableSpawnEvent = "minecraft:entity_spawned";
