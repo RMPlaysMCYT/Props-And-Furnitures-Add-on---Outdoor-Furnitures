ee0117d0112ad0fd645b5aec5c534b35e81b3e94
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-23 14:14:21 +0800

    Add 2 New blocks for today and there are new stuff will be added later

---
+++ scripts/OutdoorSleepingChairComponent.ts
@@ -0,0 +1,107 @@
+import {
+  BlockComponentPlayerInteractEvent,
+  BlockComponentTickEvent,
+  BlockCustomComponent,
+  world,
+  system,
+  Entity,
+  PlayAnimationOptions,
+  Player,
+  Block
+} from "@minecraft/server";
+import * as btch from "@minecraft/server";
+import * as vec3 from "./vec3";
+
+type CardinalDirection = "north" | "east" | "south" | "west";
+
+const Sittable_ACTOR_TYPE = "pafa:pafa_seat2";
+
+const SLEEPANIMATIONS = {
+  sleep: "animation.player.sleeping"
+}
+
+const onPlayerInteract = (arg1: BlockComponentPlayerInteractEvent, arg2: btch.CustomComponentParameters): void => {
+  const { block, dimension, player } = arg1;
+  const params = arg2.params as any;
+
+  if (!player) return;
+
+  const centro = block.center();
+
+  if (vec3.distance(centro, player.location) > 2.5) {
+    if (block.hasTag("outdoor_chair")) {
+      player.onScreenDisplay.setActionBar({
+        translate: "outdoor_chair.out_of_range",
+      });
+    }
+    return;
+  }
+  const centralHeightY = centro.y;
+
+  let height: number;
+  try {
+    height = centralHeightY + params.height;
+  } catch {
+    height = centralHeightY + 0.1;
+  }
+
+  const ChairLocationes = {
+    x: centro.x,
+    y: height,
+    z: centro.z,
+  };
+
+  const ChairSittingPosition =
+    dimension.getEntities({
+      type: Sittable_ACTOR_TYPE,
+      closest: 1,
+      maxDistance: 0.5,
+      location: ChairLocationes,
+    }).length > 0;
+
+  if (ChairSittingPosition) return;
+
+  const blockDirectiones = block.permutation.getState("minecraft:cardinal_direction") as CardinalDirection;
+
+  let SittableSpawnEvent: string;
+
+  switch (blockDirectiones) {
+    case "north":
+      SittableSpawnEvent = `sittable_actor:r180`;
+      break;
+    case "east":
+      SittableSpawnEvent = `sittable_actor:r270`;
+      break;
+    case "west":
+      SittableSpawnEvent = `sittable_actor:r90`;
+      break;
+    default:
+      SittableSpawnEvent = "minecraft:entity_spawned";
+      break;
+  }
+
+  const sittable_anchorArea = dimension.spawnEntity(Sittable_ACTOR_TYPE, ChairLocationes, {
+    spawnEvent: SittableSpawnEvent,
+  });
+
+  system.run(() => {
+    const rideable = sittable_anchorArea.getComponent("rideable");
+    if (!rideable) return;
+    rideable.addRider(player);
+  });
+};
+
+// world.afterEvents.dataDrivenEntityTrigger.subscribe(({entity}) => {
+//   try {
+//     if (!entity) return;
+//     const player = entity?.getComponent("rideable")?.getRiders();
+//     if (!player) return;
+//     player.playAnimation(SLEEPANIMATIONS.sleep, {stop });
+//   } catch (error){}
+// })
+
+system.beforeEvents.startup.subscribe((InitEvent) => {
+  InitEvent.blockComponentRegistry.registerCustomComponent("pafa_outdoor:ChairInteract2", {
+    onPlayerInteract,
+  });
+});
